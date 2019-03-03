9143550123d9915c8c55f791ed86a539cf1c2ce8
MadhuriGujjar001 <madhurigujjar01@gmail.com>
2019-03-03 15:40:59 -0600

    Git commands updated

--- src/test/java/TestNG/GoogleTests/GoogleImageSearchTest.java
+++ src/test/java/TestNG/GoogleTests/GoogleImageSearchTest.java
@@ -23,6 +23,7 @@ public class GoogleImageSearchTest extends BaseTestCase{
     @BeforeMethod
     public void setUp() throws Exception {
         driver = DriverFactory.getDriver(DriverFactory.Browsers.FIREFOX);
+        // test git commands
         driver.get("http://www.google.com");
     }
 
